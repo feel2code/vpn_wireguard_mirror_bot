a5f2f4165794f84d2619be138df2d8df19971df5
Feliks Nabiullin <fnabiullin@fxcintel.com>
2025-09-01 21:57:00 +0300

    delete proxy automation

---
+++ delete_proxy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "No argument provided"
+    exit 1
+fi
+
+FILE="/etc/3proxy/.proxyauth"
+chmod 640 $FILE
+
+
+echo "$arg1:CL:$arg2" >> "$output_file"
+
+
+
+UUID="$1"
+
+if [ ! -f "$FILE" ]; then
+    echo "File '$FILE' does not exist"
+    exit 1
+fi
+
+TMP_FILE=$(mktemp)
+
+grep -v "^${UUID}:" "$FILE" > "$TMP_FILE"
+
+if cmp -s "$FILE" "$TMP_FILE"; then
+    echo "Client UUID '$UUID' not found in file '$FILE'"
+    rm "$TMP_FILE"
+    exit 0
+fi
+
+mv "$TMP_FILE" "$FILE"
+rm "$TMP_FILE"
+
+echo "Clien UUID '$UUID' deleted from file '$FILE', restarting 3proxy..."
+
+chmod 400 $FILE
+
+sudo systemctl daemon-reload
+sudo systemctl restart 3proxy
+
+echo "Client deleted from $output_file"
+
